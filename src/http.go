525dbf1b027afa75aa8a807a91dafa851688584d
jdavanne <jdavanne@axway.com>
2017-10-25 23:19:01 +0200

    fixed http parsing

--- src/http.go
+++ src/http.go
@@ -5,11 +5,12 @@ import (
 	"regexp"
 )
 
+var re = regexp.MustCompile("(?s)^[^\r\n]+HTTP/1.1.*\r\n(?i)Host: *(\\S+) *\r\n")
+
 func GetHostnameHTTP(data string) (string, error) {
-	re := regexp.MustCompile("\r\n(?i)Host: *(\\S+) *\r\n")
 	hosts := re.FindAllStringSubmatch(data, -1)
 	if len(hosts) > 0 {
 		return hosts[0][1], nil
 	}
-	return "", errors.New("Host header not found")
+	return "", errors.New("HTTP/1.1 Host header not found")
 }
--- src/http_test.go
+++ src/http_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func ExampleNormal() {
+func ExampleGetHostnameHTTP() {
 	host, _ := GetHostnameHTTP("GET /host HTTP/1.1\r\nHost: zouzou\r\n\r\n")
 	fmt.Println(host)
 	// Output: zouzou
@@ -17,8 +17,20 @@ func ExampleShiftHost() {
 	// Output: zouzou
 }
 
+func ExampleHostCase() {
+	host, _ := GetHostnameHTTP("GET /host HTTP/1.1\r\nContent-Type: application/json\r\nhOST: zouzou\r\n\r\n")
+	fmt.Println(host)
+	// Output: zouzou
+}
+
 func TestHostMissing(t *testing.T) {
 	_, err := GetHostnameHTTP("GET /host HTTP/1.1\r\nContent-Type: application/json\r\n\r\n")
+	if err == nil {
+		t.Errorf("Should be error")
+	}
+}
+func TestNoyHTTP11(t *testing.T) {
+	_, err := GetHostnameHTTP("GET /host HTTP/1.0\r\nContent-Type: application/json\r\nHost: zouzou\r\n\r\n")
 	if err == nil {
 		t.Errorf("Should be error")
 	}
